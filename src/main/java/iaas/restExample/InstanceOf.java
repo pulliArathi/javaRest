a2979d9ffed8138cf3e69b6240e35bdc9703cec4
pulliArathi <arathipulli2426@gmail.com>
2022-11-07 13:10:12 +0530

    InstanceOf and trainee updated

---
+++ src/main/java/iaas/restExample/InstanceOf.java
@@ -0,0 +1,24 @@
+package iaas.restExample;
+
+public class InstanceOf<T> {
+public T x;
+	T ar[];
+	public String getType() {
+		if(x instanceof Integer)
+			return "int";
+		else if(x instanceof Float)
+			return "decimal number";
+		else if(x instanceof String)
+			return "text";
+		else
+			return "---";
+	}
+	
+	public void createArray(T... values) {
+		ar=values;
+	}
+	
+	public T[] readArray() {
+		return ar;
+	}
+}
